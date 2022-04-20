6673c6699530b6e071b651033884db51f9cc4cfe
anuragparla <anuragparla@gmail.com>
2022-04-20 10:52:17 -0400

    Updated data.js

--- husky-estate/src/data.js
+++ husky-estate/src/data.js
@@ -94,7 +94,7 @@ export const SocialMediaLinks = [
 export const HomesForRent = [
   {
     id: 20,
-    img: "https://photos.zillowstatic.com/fp/ca76b2e8c896037464e94f83c3efaaea-p_e.jpg",
+    img: "home1.jpeg",
     price: "$2,050",
     beds: 2,
     baths: 1,
@@ -106,7 +106,7 @@ export const HomesForRent = [
   },
   {
     id: 21,
-    img: "https://photos.zillowstatic.com/fp/d1c5c207d573ccee8bbfed7bbf06774c-p_e.jpg",
+    img: "home1.jpeg",
     price: "$3,550",
     beds: 2,
     baths: 1,
@@ -118,7 +118,7 @@ export const HomesForRent = [
   },
   {
     id: 22,
-    img: "https://photos.zillowstatic.com/fp/1da99ee239e4c03e1f1fb8e657d349b6-p_e.jpg",
+    img: "home1.jpeg",
     price: "$4,550",
     beds: 2,
     baths: 1,
@@ -130,7 +130,7 @@ export const HomesForRent = [
   },
   {
     id: 23,
-    img: "https://photos.zillowstatic.com/fp/36b37a368492658e91c91403e1bcc96d-p_e.jpg",
+    img: "home1.jpeg",
     price: "$5,550",
     beds: 2,
     baths: 1,
@@ -142,7 +142,7 @@ export const HomesForRent = [
   },
   {
     id: 24,
-    img: "https://photos.zillowstatic.com/fp/00e0ef87f09476526a79ab5e34eb5317-p_e.jpg",
+    img: "home1.jpeg",
     price: "3,105",
     beds: 2,
     baths: 1,
@@ -154,7 +154,7 @@ export const HomesForRent = [
   },
   {
     id: 25,
-    img: "https://photos.zillowstatic.com/fp/dbb22740fa48c1607d903ec2c8f1d776-p_e.jpg",
+    img: "home1.jpeg",
     price: "$4,250",
     beds: 2,
     sqft: 4323,
@@ -166,7 +166,7 @@ export const HomesForRent = [
   },
   {
     id: 26,
-    img: "https://photos.zillowstatic.com/fp/6754b60d08af9a52c28eb710681784c8-p_e.jpg",
+    img: "home1.jpeg",
     price: "$2,350",
     beds: 2,
     baths: 1,
@@ -178,7 +178,7 @@ export const HomesForRent = [
   },
   {
     id: 27,
-    img: "https://photos.zillowstatic.com/fp/7150d54ac73468c4b5c34c9e6dd698af-p_e.jpg",
+    img: "home1.jpeg",
     price: "$5,550",
     beds: 2,
     baths: 1,
@@ -190,7 +190,7 @@ export const HomesForRent = [
   },
   {
     id: 28,
-    img: "https://photos.zillowstatic.com/fp/d0e016a1358e5a5667f780bb4b7fb6fe-p_e.jpg",
+    img: "home1.jpeg",
     price: "$4,650",
     beds: 2,
     sqft: 4323,
@@ -202,7 +202,7 @@ export const HomesForRent = [
   },
   {
     id: 29,
-    img: "https://photos.zillowstatic.com/fp/55ac0e9ac975438b19c5d8ae85a988bf-p_e.jpg",
+    img: "home1.jpeg",
     price: "$3,980",
     beds: 2,
     baths: 1,
@@ -217,7 +217,7 @@ export const HomesForRent = [
 export const HomesForSale = [
   {
     id: 20,
-    img: "https://photos.zillowstatic.com/fp/ca76b2e8c896037464e94f83c3efaaea-p_e.jpg",
+    img: "home1.jpeg",
     price: "$1550",
     beds: 2,
     baths: 1,
@@ -228,7 +228,7 @@ export const HomesForSale = [
   },
   {
     id: 21,
-    img: "https://photos.zillowstatic.com/fp/d1c5c207d573ccee8bbfed7bbf06774c-p_e.jpg",
+    img: "home1.jpeg",
     price: "$1550",
     beds: 2,
     baths: 1,
@@ -239,7 +239,7 @@ export const HomesForSale = [
   },
   {
     id: 22,
-    img: "https://photos.zillowstatic.com/fp/1da99ee239e4c03e1f1fb8e657d349b6-p_e.jpg",
+    img: "home1.jpeg",
     price: "$1550",
     beds: 2,
     baths: 1,
@@ -250,7 +250,7 @@ export const HomesForSale = [
   },
   {
     id: 23,
-    img: "https://photos.zillowstatic.com/fp/36b37a368492658e91c91403e1bcc96d-p_e.jpg",
+    img: "home1.jpeg",
     price: "$1550",
     beds: 2,
     baths: 1,
@@ -261,7 +261,7 @@ export const HomesForSale = [
   },
   {
     id: 24,
-    img: "https://photos.zillowstatic.com/fp/00e0ef87f09476526a79ab5e34eb5317-p_e.jpg",
+    img: "home1.jpeg",
     price: "$1550",
     beds: 2,
     baths: 1,
@@ -272,7 +272,7 @@ export const HomesForSale = [
   },
   {
     id: 25,
-    img: "https://photos.zillowstatic.com/fp/dbb22740fa48c1607d903ec2c8f1d776-p_e.jpg",
+    img: "home1.jpeg",
     price: "$1550",
     beds: 2,
     baths: 1,
@@ -283,7 +283,7 @@ export const HomesForSale = [
   },
   {
     id: 26,
-    img: "https://photos.zillowstatic.com/fp/6754b60d08af9a52c28eb710681784c8-p_e.jpg",
+    img: "home1.jpeg",
     price: "$1550",
     beds: 2,
     baths: 1,
@@ -294,7 +294,7 @@ export const HomesForSale = [
   },
   {
     id: 27,
-    img: "https://photos.zillowstatic.com/fp/7150d54ac73468c4b5c34c9e6dd698af-p_e.jpg",
+    img: "home1.jpeg",
     price: "$1550",
     beds: 2,
     baths: 1,
@@ -305,7 +305,7 @@ export const HomesForSale = [
   },
   {
     id: 28,
-    img: "https://photos.zillowstatic.com/fp/d0e016a1358e5a5667f780bb4b7fb6fe-p_e.jpg",
+    img: "home1.jpeg",
     price: "$1550",
     beds: 2,
     baths: 1,
@@ -316,7 +316,7 @@ export const HomesForSale = [
   },
   {
     id: 29,
-    img: "https://photos.zillowstatic.com/fp/55ac0e9ac975438b19c5d8ae85a988bf-p_e.jpg",
+    img: "home1.jpeg",
     price: "$1550",
     beds: 2,
     baths: 1,
