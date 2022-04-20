2459926de83d865e8ffee029e21213b2a238d80c
anuragparla <anuragparla@gmail.com>
2022-04-20 10:54:42 -0400

    Updated App.js

--- husky-estate/src/App.js
+++ husky-estate/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
+import HomesForSale from "./Pages/HomesForSale";
+// import HomesForRent from "./Pages/HomesForRent";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" exact element={<HomePage />} />
+        <Route exact path="/homesForSale" element={<HomesForSale />} />
+        {/* <Route exact path="/homesForRent" element={<HomesForRent />} /> */}
       </Routes>
     </Router>
   );
